b73bb57cb336889790ade4522cce6969483df080
danirubio15 <danirubio15@elpuig.xeill.net>
2022-04-21 10:57:22 +0200

    2

---
+++ main.js
@@ -0,0 +1,24 @@
+
+fetch("./menu.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("menu").innerHTML = data;
+  });
+
+fetch("./footer.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("footer").innerHTML = data;
+  });
+
+  fetch("./content.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("content").innerHTML = data;
+    });
